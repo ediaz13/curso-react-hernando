42a1665cc5dbaee38d128c26897b18187804dd87
ediaz13 <diazemanuel27@gmail.com>
2023-06-30 21:06:31 -0300

    18. Desestructuación de Objetos

---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,22 @@
+const persona = {
+  nombre: 'Tony',
+  edad: 45,
+  clave: 'Ironman'
+};
+
+const locve =  ({ clave, nombre, edad, rango = 'Capitan'}) => {
+
+  return {
+    nombreClave: clave,
+    anios:edad,
+    latlng: {
+      lat:14.1232,
+      lng: -12.3232
+    }
+  }
+}
+
+const { nombreClave, anios, latlng:{ lat, lng} } = locve( persona );
+
+console.log( nombreClave, anios );
+console.log( lat, lng);
