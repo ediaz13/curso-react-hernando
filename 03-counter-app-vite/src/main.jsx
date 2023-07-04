b4c0dfe5fc61a8b9e33ae01bc8471d86f5844bec
ediaz13 <diazemanuel27@gmail.com>
2023-07-04 19:17:33 -0300

    43. Tarea - Componente CounterApp

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import { HelloWorldApp } from './HelloWorldApp';
 import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
+
 
 import './styles.css'
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <FirstApp title= "New terminal" />
+        <CounterApp value= {10} />
     </React.StrictMode>
 );
