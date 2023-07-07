67c881dbb68d54733799c42faf7eba20a08157a7
ediaz13 <diazemanuel27@gmail.com>
2023-07-07 17:41:00 -0300

    53. Jest - Expect - toBe

--- 03-counter-app-vite/test/demo.test.js
+++ 03-counter-app-vite/test/demo.test.js
@@ -1,8 +1,14 @@
 
-
-
-test('Esta prueba no debe fallar', () => {
-    if (1 === 0) {
-        throw new Error('No se puede dividir por cero!');
-    }
-})
+describe('Pruebas en <DemoComponent>', () => { 
+    test('Esta prueba no debe fallar', () => {
+    
+        //1. Inicializacion.
+        const message1 = 'Hola mundo';
+    
+        //2. Estimulo.
+        const message2 = message1.trim();
+    
+        //3. Observar el comportamiento.
+        expect( message1 ).toBe( message2 );
+    });
+});
