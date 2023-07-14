4ecb41fbaabb976c3e36c4b26079551252e6d9ab
ediaz13 <diazemanuel27@gmail.com>
2023-07-14 18:46:58 -0300

    103. Pruebas del componente - GifGridItem

--- 04-gif-expert-app/tests/components/GifItem.test.jsx
+++ 04-gif-expert-app/tests/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { GifItem } from "../../src/components/GifItem";
 
 describe('Pruebas de <GiftItem /> ', () => {
@@ -10,5 +10,23 @@ describe('Pruebas de <GiftItem /> ', () => {
         const { container } = render( <GifItem title={ title } url={ url } /> );
 
         expect( container ).toMatchSnapshot();
+    })
+
+    test('Debe mostrar la imagen con el URL y el ALT indicado', () => {
+        render( <GifItem title={ title } url={ url } /> );
+        
+        //expect( screen.getRole('img').src ).toBe( url );
+        //expect( screen.getRole('src').src ).toBe( title );
+
+        const { src, alt } = screen.getByRole('img');
+        expect( src ).toBe( url );
+        expect( alt ).toBe( title );
+    })
+
+    test('Debe mostrar el titulo del comoponente', () => {
+        render( <GifItem title={ title } url={ url } /> );
+        expect( screen.getByText( title ) ).toBeTruthy();
+        
+        
     })
 });
