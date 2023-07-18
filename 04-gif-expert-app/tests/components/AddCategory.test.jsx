cbd30b7cf36b0f94e4000c553d0a788e3b59542a
ediaz13 <diazemanuel27@gmail.com>
2023-07-18 15:21:53 -0300

    105. Pruebas del componente - AddCategory

--- 04-gif-expert-app/tests/components/AddCategory.test.jsx
+++ 04-gif-expert-app/tests/components/AddCategory.test.jsx
@@ -1,10 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 const { AddCategory } = require('../../src/components/AddCategory');
 
 
 describe('Prueba en <AddCategory />', () => {
     test('Debe de cambiar el valor de la caja de texto.', () => {
+        
         render( <AddCategory onNewCategory={ () => {} } /> );
+        const input = screen.getByRole('textbox');
+        
+        fireEvent.input( input, {target: { value: 'Jamaica' } });
+
+        expect( input.value ).toBe('Jamaica');
         screen.debug();
     });
 
