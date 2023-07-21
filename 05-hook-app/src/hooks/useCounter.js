d35cc3cefe9943cac98839e4746c4b020cea69b6
ediaz13 <diazemanuel27@gmail.com>
2023-07-21 19:07:52 -0300

    118. Exponer métodos del Hook

--- 05-hook-app/src/hooks/useCounter.js
+++ 05-hook-app/src/hooks/useCounter.js
@@ -4,9 +4,24 @@ const useCounter = ( initialValue = 10 ) => {
 
     const [counter, setCounter] = useState(initialValue);
 
+    const increment = ( value = 1 ) => {
+        setCounter( counter + value );
+    }
+
+    const decrement = ( value = 1 ) => {
+        setCounter( counter - value );
+    }
+
+    const reset = () => {
+        setCounter( initialValue );
+    }
+
     return {
         counter,
+        increment,
+        decrement,
+        reset,
     }
 }
 
-export default useCounter
+export default useCounter;
