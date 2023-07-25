bd3d8f26fcac55494318b09dde6abde968ad0964
ediaz13 <diazemanuel27@gmail.com>
2023-07-24 21:18:36 -0300

    122. useEffect - Precauciones

--- 05-hook-app/src/02-useEffect/Message.jsx
+++ 05-hook-app/src/02-useEffect/Message.jsx
@@ -4,10 +4,17 @@ import { useEffect } from "react"
 export const Mesagge = () => {
 
   useEffect(() => {
-    console.log('Message Mounted!');
+
+    const onMouseMove = ({ x, y }) => {
+      const coords = { x, y };
+      console.log( coords );
+    }
+
+    window.addEventListener( 'mousemove', onMouseMove );
   
     return () => {
-      console.log('Message Unmounted!');
+      window.removeEventListener( 'mousemove', onMouseMove );
+    
     }
   }, [])
   
